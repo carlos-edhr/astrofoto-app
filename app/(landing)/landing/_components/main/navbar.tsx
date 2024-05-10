ba621f9988b8cd1d1b3e1f30698cb38b7bb1489e
Eduardo Rojas <carlos.edhr@protonmail.com>
2024-05-09 23:14:27 -0700

    Hero section title changes

--- app/(landing)/landing/_components/main/navbar.tsx
+++ app/(landing)/landing/_components/main/navbar.tsx
@@ -11,27 +11,30 @@ export const NavBar = () => {
           className="h-auto w-auto flex flex-row items-center"
         >
           <Image
-            src="/NavLogo.png"
+            src="/vector-3.svg"
             alt="logo"
-            width={70}
-            height={70}
+            width={40}
+            height={40}
             className="cursor-pointer hover:animate-slowspin"
           />
           <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-            WebChain Dev
+            Astrofoto
           </span>
         </a>
 
         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
             <a href="#about-me" className="cursor-pointer">
-              About me
+              Acerca del congreso
             </a>
             <a href="#skills" className="cursor-pointer">
-              Skills
+              Participantes
             </a>
             <a href="#projects" className="cursor-pointer">
-              Projects
+              Galería
+            </a>
+            <a href="#projects" className="cursor-pointer">
+              Contacto
             </a>
           </div>
         </div>
--- app/(landing)/landing/_components/sub/HeroContent.tsx
+++ app/(landing)/landing/_components/sub/HeroContent.tsx
@@ -16,43 +16,42 @@ const HeroContent = () => {
     <motion.div
       initial="hidden"
       animate="visible"
-      className="flex flex-row items-center justify-center px-20 mt-40 mb-40 w-full h-full z-[20]"
+      className="flex flex-row items-center justify-center px-20   w-full h-full z-[20]"
     >
-      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
-        <motion.div
+      <div className="h-full w-full pl-4 flex flex-col gap-5 justify-center m-auto text-start">
+        {/*  <motion.div
           variants={slideInFromTop}
           className="Welcome-box py-[8px] px-[17px] border border-[#7042f88b] opacity-[0.9]"
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px] bg-gradient-to-r from-sky-200 to-sky-600 animatedgradient">
             Congreso de Astrofotografía
-          </h1>
-        </motion.div>
+          </h1> 
+        </motion.div>*/}
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
+          className="flex flex-col gap-6 mt-9 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Congreso{" "}
-            <span className="font-['Maitree'] mb-4 text-4xl font-bold leading-none tracking-tight  md:text-5xl lg:text-6xl xt-wdark:tehite bg-gradient-to-l pt-5 md:mt-5 from-pink-700 via-rose-500 to-red-400 text-transparent bg-clip-text bg-300% animate-gradient">
-              the best{" "}
+            Congreso Internacional de{" "}
+            <span className="font-['Maitree'] mb-4 text-4xl font-bold leading-none tracking-tight  md:text-5xl lg:text-6xl xt-wdark:tehite bg-gradient-to-l pt-5 md:mt-5 from-sky-200 to-sky-600 text-transparent bg-clip-text bg-300% animate-gradient">
+              Astrofotografía{" "}
             </span>
-            project experience
           </span>
         </motion.div>
         <motion.p
           variants={slideInFromLeft(0.8)}
-          className="text-lg text-gray-400 my-5 max-w-[600px]"
+          className="text-lg text-gray-400 max-w-[600px]"
         >
-          El congreso Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Repudiandae a optio iure cumque quibusdam maxime odit deleniti iusto
-          hic.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae
+          a optio iure cumque quibusdam maxime odit deleniti iusto hic.
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
-          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+          className="flex flex-row items-center justify-center py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
         >
-          Learn more!
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          ¡Regístrate!
         </motion.a>
       </div>
       <div className="hidden w-full h-full md:block  justify-center items-center">
