9623064aaf4799cd7b2e9fbf6c88c3df99ee19fc
Eduardo Rojas <carlos.edhr@protonmail.com>
2024-06-26 01:03:10 -0700

    Attempting to fix after Authentication redirect issue 2

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,34 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export const runtime = "nodejs";
+// export const runtime = "nodejs";
 
 export default authMiddleware({
   publicRoutes: [
     "/",
     "/landing",
-    "/sign-in(.*)",
-    "/sign-up(.*)",
+    // "/sign-in(.*)",
+    // "/sign-up(.*)",
     "/api/webhooks(.*)",
     "/api/uploadthing",
     "/:username",
     "/search",
   ],
-  async afterAuth(auth, req) {
-    if (auth.isPublicRoute) {
-      //  For public routes, we don't need to do anything
-      return NextResponse.next();
-    }
+  // async afterAuth(auth, req) {
+  //   if (auth.isPublicRoute) {
+  //     //  For public routes, we don't need to do anything
+  //     return NextResponse.next();
+  //   }
 
-    const url = new URL(req.nextUrl.origin);
+  //   const url = new URL(req.nextUrl.origin);
 
-    if (!auth.userId) {
-      //  If user tries to access a private route without being authenticated,
-      //  redirect them to the sign in page
-      url.pathname = "/sign-in";
-      return NextResponse.redirect(url);
-    }
-  },
+  //   if (!auth.userId) {
+  //     //  If user tries to access a private route without being authenticated,
+  //     //  redirect them to the sign in page
+  //     url.pathname = "/sign-in";
+  //     return NextResponse.redirect(url);
+  //   }
+  // },
 });
 
 export const config = {
