b53030fb61f789d742a716e3d2122d7e4cd1f1ab
Eduardo Rojas <carlos.edhr@protonmail.com>
2025-01-24 23:51:29 -0800

    Improved the design and layout of Itinerario component

--- app/(landingpage)/(home)/_components/nuevo-itinerario.tsx
+++ app/(landingpage)/(home)/_components/nuevo-itinerario.tsx
@@ -1,18 +1,31 @@
 import { FC } from "react";
 import Image from "next/image";
+import StarBackground from "@/app/(landing)/(home)/_components/main/StarBackground";
 
 const Itinerario: FC = () => {
   return (
-    <section className="relative w-full bg-gradient-to-br from-[#062945] via-[#030916] to-[#020814]">
+    <section className="relative w-full bg-gradient-to-br from-[#062945] via-[#030916] to-[#020814] ">
       {/* Optional: You could add a dark overlay if needed, e.g.: */}
-      <div className="absolute inset-0 bg-[#181818] bg-opacity-20" />
+      {/* <div className="absolute inset-0 bg-[#181818] bg-opacity-20" /> */}
 
       {/* Main content container */}
       <div className="relative mx-auto max-w-7xl px-4 py-8 md:py-12 lg:py-16">
+        <div
+          className="absolute inset-0 bg-gradient-radial from-orange-500/40 via-orange-500/20 to-transparent rounded-full filter blur-3xl"
+          style={{
+            width: "650px",
+            height: "650px",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            zIndex: 50,
+            opacity: 0.6,
+          }}
+        ></div>
         {/* Top row: headings + small collage  */}
         <div className="relative flex flex-col md:flex-row">
           <div className="flex-1">
-            <h1 className="text-slate-200 text-6xl sm:text-7xl lg:text-8xl font-bold leading-none">
+            <h1 className="special-font section-heading   text-6xl sm:text-7xl lg:text-8xl font-bold leading-none">
               Itinerario
             </h1>
           </div>
@@ -22,7 +35,7 @@ const Itinerario: FC = () => {
                 src="/CIAF7-D3-T2 Brandon.jpg"
                 alt="Nepal Girls"
                 fill
-                className="object-cover"
+                className="object-cover rounded-full"
               />
             </div>
             <div className="relative w-[120px] h-[120px]">
@@ -30,7 +43,7 @@ const Itinerario: FC = () => {
                 src="/CIAF7-D3-T3 Juan.jpg"
                 alt="Yak"
                 fill
-                className="object-cover"
+                className="object-cover rounded-full"
               />
             </div>
             <div className="relative w-[120px] h-[120px]">
@@ -38,7 +51,7 @@ const Itinerario: FC = () => {
                 src="/CIAF7-D3-T4 Yuri.jpg"
                 alt="Mountains"
                 fill
-                className="object-cover"
+                className="object-cover rounded-full"
               />
             </div>
           </div>
@@ -71,24 +84,24 @@ const Itinerario: FC = () => {
             {/* Left side: article list */}
             <div className="md:w-1/2 space-y-6">
               <ArticleItem
-                date="01.03.24"
-                title="THAT MAGICAL PLACE CALLED NEPAL"
+                date="06.03.25"
+                title="EL IMPACTO DE LA ASTROFOTOGRAFÍA EN LA INVESTIGACIÓN"
               />
               <ArticleItem
-                date="01.03.24"
-                title="A NIGHT FULL ADVENTURE IN GRAND CANYON"
+                date="06.03.25"
+                title="EL IMPACTO DE LA ASTROFOTOGRAFÍA EN LA INVESTIGACIÓN"
               />
               <ArticleItem
-                date="01.03.24"
-                title="DISCOVER A HIDDEN PARADISE IN WESTERN INDONESIA"
+                date="06.03.25"
+                title="EL IMPACTO DE LA ASTROFOTOGRAFÍA EN LA INVESTIGACIÓN"
               />
               <ArticleItem
-                date="01.03.24"
-                title="THAT MAGICAL PLACE CALLED NEPAL"
+                date="06.03.25"
+                title="EL IMPACTO DE LA ASTROFOTOGRAFÍA EN LA INVESTIGACIÓN"
               />
               <ArticleItem
-                date="01.03.24"
-                title="A NIGHT FULL ADVENTURE IN GRAND CANYON"
+                date="06.03.25"
+                title="EL IMPACTO DE LA ASTROFOTOGRAFÍA EN LA INVESTIGACIÓN"
               />
             </div>
 
@@ -100,12 +113,12 @@ const Itinerario: FC = () => {
                   alt="Invitados Poster"
                   width={350}
                   height={400}
-                  className="object-cover"
+                  className="object-cover rounded"
                 />
               </div>
               <div className="mt-2 text-slate-200">
-                <p className="font-semibold">by Samantha William</p>
-                <p className="text-sm">FULLTIME TRAVELLER</p>
+                <p className="font-semibold">CONGRESO INTERNACIONAL</p>
+                <p className="text-sm">de ASTROFOTOGRAFÍA</p>
               </div>
             </div>
           </div>
@@ -119,7 +132,7 @@ const Itinerario: FC = () => {
               src="/CIAF7-D1-01 Roberto.jpg"
               alt="Example 1"
               fill
-              className="object-cover"
+              className="object-cover rounded-2xl"
             />
           </div>
           <div className="relative w-full h-64">
@@ -127,7 +140,7 @@ const Itinerario: FC = () => {
               src="/CIAF7-D1-02 Antoni.jpg"
               alt="Example 2"
               fill
-              className="object-cover"
+              className="object-cover rounded-2xl"
             />
           </div>
           <div className="relative w-full h-64">
@@ -135,11 +148,94 @@ const Itinerario: FC = () => {
               src="/CIAF7-D1-04 Daniela.jpg"
               alt="Example 3"
               fill
-              className="object-cover"
+              className="object-cover rounded-2xl"
             />
           </div>
         </div>
+        {/* PLACE GLASS CARD HERE --START HERE -- */}
+        <div
+          className="
+            mt-10 
+            p-6 
+            rounded-xl 
+            bg-white/10 
+            backdrop-blur-md 
+            border 
+            border-white/20 
+            shadow-lg 
+            text-slate-200
+          "
+        >
+          <div className="flex flex-col md:flex-row">
+            {/* Left side: main portrait/poster + author label */}
+            <div className="relative md:w-1/2 mt-8 md:mt-0 md:pl-8">
+              <div className="w-[250px] md:w-[350px] h-auto">
+                <Image
+                  src="/2024-CIAF7_Invitados.jpg"
+                  alt="Invitados Poster"
+                  width={350}
+                  height={400}
+                  className="object-cover"
+                />
+              </div>
+              <div className="mt-2 text-slate-200">
+                <p className="font-semibold">CONGRESO INTERNACIONAL</p>
+                <p className="text-sm">de ASTROFOTOGRAFÍA</p>
+              </div>
+            </div>
+
+            {/* Right side: article list */}
+            <div className="md:w-1/2 space-y-6">
+              <ArticleItem
+                date="06.03.25"
+                title="EL IMPACTO DE LA ASTROFOTOGRAFÍA EN LA INVESTIGACIÓN"
+              />
+              <ArticleItem
+                date="06.03.25"
+                title="EL IMPACTO DE LA ASTROFOTOGRAFÍA EN LA INVESTIGACIÓN"
+              />
+              <ArticleItem
+                date="06.03.25"
+                title="EL IMPACTO DE LA ASTROFOTOGRAFÍA EN LA INVESTIGACIÓN"
+              />
+              <ArticleItem
+                date="06.03.25"
+                title="EL IMPACTO DE LA ASTROFOTOGRAFÍA EN LA INVESTIGACIÓN"
+              />
+              <ArticleItem
+                date="06.03.25"
+                title="EL IMPACTO DE LA ASTROFOTOGRAFÍA EN LA INVESTIGACIÓN"
+              />
+            </div>
+          </div>
+        </div>
+        {/* PLACE GLASS CARD HERE --END HERE -- */}
       </div>
+
+      {/* <div
+        className="
+          absolute
+          inset-0
+          m-auto
+          p-6 
+          rounded-xl 
+          
+          z-10
+          flex
+          items-center
+          justify-center
+        "
+        style={{
+          top: "0",
+          bottom: "0",
+          left: "0",
+          right: "0",
+          height: "fit-content",
+          width: "fit-content",
+        }}
+      >
+        <StarBackground />
+      </div> */}
     </section>
   );
 };
@@ -153,8 +249,10 @@ interface ArticleItemProps {
 }
 const ArticleItem: FC<ArticleItemProps> = ({ date, title }) => (
   <div className="border-b border-gray-200 pb-2">
-    <p className="text-red-500 font-semibold text-sm">{date}</p>
-    <h3 className="text-red-500 font-bold text-lg leading-tight mt-1">
+    <p className="text-slate-500 text-muted-foreground font-semibold text-sm">
+      {date}
+    </p>
+    <h3 className="text-slate-300 font-bold text-lg leading-tight mt-1">
       {title}
     </h3>
   </div>
